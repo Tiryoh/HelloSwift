7d98431842ad69d4aa6fc71f7a1c894ab0613063
Tiryoh <tiryoh@gmail.com>
2015-11-03 18:03:06 +0900

    Add mathmatical calculation uses variable

---
+++ VariableMath.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+var num1 = 62
+var num2 = 10
+
+print("\(num1) + \(num2) = \(num1 + num2)") 
+print("\(num1) - \(num2) = \(num1 - num2)") 
+print("\(num1) * \(num2) = \(num1 * num2)") 
+print("\(num1) / \(num2) = \(num1 / num2)") 
+print("\(num1) % \(num2) = \(num1 % num2)")
