23c650489505bf0478714fc551c2f076d5cd6413
Tiryoh <tiryoh@gmail.com>
2015-11-03 17:20:08 +0900

    Add Mathmatical calculation

---
+++ Math.swift
@@ -0,0 +1,14 @@
+//
+//  Math
+//
+//  Created by Tiryoh on 11/03/15.
+
+import Foundation
+
+print("3 + 6 = \(3 + 6)")
+print("12 - 4 = \(12 - 4)")
+print("7 * 3 = \(7 * 3)")
+print("8 / 3 = \(8 / 3)")
+print("8 % 3 = \(8 % 3)")
+print("3 + 5 * 2 = \(3 + 5 * 2)")
+print("(3 + 5) * 2 = \((3 + 5) * 2)")
